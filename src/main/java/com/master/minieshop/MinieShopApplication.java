67130320abfc08b22f628b054c9d6d19e037caf1
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-05-07 20:38:59 +0700

    Initial commit

---
+++ src/main/java/com/master/minieshop/MinieShopApplication.java
@@ -0,0 +1,13 @@
+package com.master.minieshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinieShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinieShopApplication.class, args);
+	}
+
+}
