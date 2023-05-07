086e8469fba48845740aba125b83fafe06ab4f83
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-05-07 20:49:07 +0700

    fix: Update

--- src/test/java/com/master/minieshop/MinieShopApplicationTests.java
+++ src/test/java/com/master/minieshop/MinieShopApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MinieShopApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
