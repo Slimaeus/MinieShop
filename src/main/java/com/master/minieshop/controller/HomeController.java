bd0330b638d03b73f19d3460821733c84956370b
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-05-14 16:57:11 +0700

    ⭐Add Home controller and update Product controller

---
+++ src/main/java/com/master/minieshop/controller/HomeController.java
@@ -0,0 +1,9 @@
+package com.master.minieshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/{controller}")
+public class HomeController {
+}
--- src/main/java/com/master/minieshop/controller/ProductsController.java
+++ src/main/java/com/master/minieshop/controller/ProductsController.java
@@ -1,4 +1,23 @@
 package com.master.minieshop.controller;
 
+import com.master.minieshop.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/{controller}")
 public class ProductsController {
+    private ProductService productService;
+
+    public ProductsController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("products", productService.getAll());
+        return "product/index";
+    }
 }
