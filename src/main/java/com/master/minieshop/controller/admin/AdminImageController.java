18fa058de24324016786efa8b419e2a9abc3ee73
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-05-25 21:53:56 +0700

    🛠 Fix AdminImageController and AdminImage views

--- src/main/java/com/master/minieshop/controller/admin/AdminImageController.java
+++ src/main/java/com/master/minieshop/controller/admin/AdminImageController.java
@@ -1,5 +1,6 @@
 package com.master.minieshop.controller.admin;
 
+import com.master.minieshop.entity.Image;
 import com.master.minieshop.service.CategoryService;
 import com.master.minieshop.service.ImageService;
 import com.master.minieshop.service.ProductService;
@@ -11,54 +12,60 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("admin/images")
 public class AdminImageController {
-    @Autowired
-    private ImageService imageService;
+    private final ImageService imageService;
+    private final ProductService productService;
+
+    public AdminImageController(ImageService imageService, ProductService productService) {
+        this.imageService = imageService;
+        this.productService = productService;
+    }
 
     @GetMapping
     public String index(Model model){
-        var currentImage = imageService.getAll();
-        model.addAttribute("Image",currentImage);
-        return "admin/Image/index";
+        model.addAttribute("images",imageService.getAll());
+        return "admin/images/index";
     }
 
     @GetMapping("/create")
-    public String Image(Model model){
-        model.addAttribute("image",new image());
-        return "admin/Image/create";
+    public String create(Model model){
+        model.addAttribute("image",new Image());
+        model.addAttribute("products", productService.getAll());
+        return "admin/images/create";
     }
 
     @PostMapping("/create")
-    public String Image(@ModelAttribute("image") image image) {
+    public String create(@ModelAttribute("image") Image image) {
         imageService.save(image);
-        return "redirect:/admin/Image";
+        return "redirect:/admin/images";
     }
 
     @GetMapping("/edit/{id}")
-    public String Image(@PathVariable("id") Integer id, Model model) {
-        image image = imageService.getById(id)
+    public String edit(@PathVariable("id") String id, Model model) {
+        Image image = imageService.getById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid image id: " + id));
         model.addAttribute("image", image);
-        return "admin/Image/edit";
+            model.addAttribute("products", productService.getAll());
+        return "admin/images/edit";
     }
 
     @PostMapping("/edit/{id}")
-    public String Image(@PathVariable("id") Integer id, @ModelAttribute("image") image image) {
+    public String edit(@PathVariable("id") String id, @ModelAttribute("image") Image image) {
         imageService.save(image);
-        return "redirect:/admin/Image";
+        return "redirect:/admin/images";
     }
 
     @GetMapping("/delete/{id}")
-    public String Image(@PathVariable("id") Integer id, Model model) {
-        image image = imageService.getById(id)
+    public String delete(@PathVariable("id") String id, Model model) {
+        Image image = imageService.getById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid image id: " + id));
         model.addAttribute("image", image);
-        return "admin/Image/delete";
+        return "admin/images/delete";
     }
 
     @PostMapping("/delete/{id}")
-    public String Image(@PathVariable("id") Integer id) {
+    public String delete(@PathVariable("id") String id) {
         imageService.deleteById(id);
-        return "redirect:/admin/Image";
+        return "redirect:/admin/images";
     }
 
 }
