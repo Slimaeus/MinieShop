cb5f078b8dbf69060a3439f67fceabc74f8e753f
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-05-25 09:15:33 +0700

    ⭐ Init View for Admin Promotions Controller

--- src/main/java/com/master/minieshop/controller/admin/AdminPromotionsController.java
+++ src/main/java/com/master/minieshop/controller/admin/AdminPromotionsController.java
@@ -1,10 +1,6 @@
 package com.master.minieshop.controller.admin;
 
-import com.master.minieshop.entity.Product;
 import com.master.minieshop.entity.Promotion;
-import com.master.minieshop.service.CategoryService;
-import com.master.minieshop.service.ImageService;
-import com.master.minieshop.service.ProductService;
 import com.master.minieshop.service.PromotionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
