583b8a9bea53e59f5b9cc29f92c040648386038c
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-05-26 12:45:33 +0700

    🧱 Update Home Controller

--- src/main/java/com/master/minieshop/controller/HomeController.java
+++ src/main/java/com/master/minieshop/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("home")
 public class HomeController {
-    @GetMapping
+    @GetMapping({"/", ""})
     public String home() {
         return "home/index";
     }
