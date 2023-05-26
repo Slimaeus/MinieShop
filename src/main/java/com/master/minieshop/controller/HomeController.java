14fa9833d614b661f1235b54613c555052875a54
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-05-26 12:32:47 +0700

    ⭐ Create Home Page and Update Layout navbar

--- src/main/java/com/master/minieshop/controller/HomeController.java
+++ src/main/java/com/master/minieshop/controller/HomeController.java
@@ -1,9 +1,15 @@
 package com.master.minieshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/{controller}")
+@RequestMapping("home")
 public class HomeController {
+    @GetMapping
+    public String home() {
+        return "home/index";
+    }
 }
