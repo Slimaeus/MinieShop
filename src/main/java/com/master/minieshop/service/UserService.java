bbe5200eda5dc712fbbf6c444548df7f5c5715cf
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-06-13 16:26:17 +0700

    🛠 refactor: Remove redundant

--- src/main/java/com/master/minieshop/service/UserService.java
+++ src/main/java/com/master/minieshop/service/UserService.java
@@ -19,17 +19,6 @@ public class UserService {
         userRepository.save(user);
     }
 
-    public void saveOauthUser(String email, @NotNull String username) {
-        if (userRepository.findByUserName(username) != null) return;
-        var user = new AppUser();
-        user.setUserName(username);
-        user.setEmail(email);
-        user.setPassword(new BCryptPasswordEncoder().encode(username));
-//        user.setProvider(Provider.GOOGLE.value);
-        user.setRole(Role.LoyalCustomer);
-        userRepository.save(user);
-    }
-
     public AppUser getById(Long id){return userRepository.getReferenceById(id);}
     public AppUser findByUsername(String username) { return userRepository.findByUserName(username);}
 }
